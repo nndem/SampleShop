c973a18df03741993e66c416b4927ec4098618be
Nickolay Demidov <nickolay.demidov@Mac-mini-Nickolay.local>
2020-10-14 17:52:42 +0300

    add redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Basket from "./components/Basket";
+import Vase from "./components/Stuff/Vase";
+import Chocolate from "./components/Stuff/Chocolate";
+import { Provider } from "react-redux";
+import initStore from "./store";
 
 function App() {
+  const store = initStore();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Basket />
+      <Vase />
+      <Chocolate />
+    </Provider>
   );
 }
 
