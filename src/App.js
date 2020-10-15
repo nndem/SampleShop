1c66f6c7918471d72cd9826606d7f19ab3793b31
Nickolay Demidov <nickolay.demidov@Mac-mini-Nickolay.local>
2020-10-15 11:51:47 +0300

    add Discount, TotalCost, Items

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ import Vase from "./components/Stuff/Vase";
 import Chocolate from "./components/Stuff/Chocolate";
 import { Provider } from "react-redux";
 import initStore from "./store";
+import TotalCount from "./components/TotalCount";
+import { BrowserRouter} from "react-router-dom";
+import {Route} from "react-router";
+import TotalCost from "./components/TotalCost";
+import ItemNameList from "./components/ItemNameList";
+import Discount from "./components/Discount";
 
 function App() {
   const store = initStore();
   return (
     <Provider store={store}>
+        <BrowserRouter>
       <Basket />
+      <TotalCount />
+      <TotalCost />
+      <Discount/>
+      <ItemNameList/>
       <Vase />
       <Chocolate />
+{/*
+          <Route path='/Buy' component={Buy}/>
+*/}
+        </BrowserRouter>
     </Provider>
   );
 }
